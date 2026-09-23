c95857eacaca76f5957d4613ca00e1d47f11c241
Vinicius Marcos <viniciusmarcospinheiro@Hotmail.com>
2026-09-22 22:05:02 -0300

    analise de valores duplicados

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# %%
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #Organização da apresentação dos dados no terminal
 pd.set_option('display.max_columns', None)
@@ -6,6 +8,24 @@ pd.set_option('display.width', None)
 
 #Importando dados do banco
 df = pd.read_csv('dataset_risco_credito_500k.csv') 
+
+# %%
+#Verificando conteúdo do banco de dados
 df.info()
 df.head()
 df.describe()
+
+
+
+# %%
+# 1. Estatísticas descritivas de tudo
+print(df.describe(include='all').T)
+
+#%%
+#Análise de dados duplicados
+print("Linhas totalmente duplicadas:", df.duplicated().sum())
+print("IDs de cliente duplicados:", df['id_cliente'].duplicated().sum())
+#vert=False → deixa a caixa deitada (mais fácil de ler quando tem outliers longe)
+
+
+# %%
